231331928d6ae42478582d9fb93b4a727317e3f5
bidenjoe1 <nathan.k.waititu@gmail.com>
2025-01-25 19:49:05 +0300

    Add a method of calculating day of thhe week and display result

--- indexscript.js
+++ indexscript.js
@@ -1,6 +1,6 @@
 const AkanForm = document.getElementById('AkanForm')
 const resultDiv = document.getElementById('result');
-const realAkanName = document.getElementById('realAkanName');
+const actualAkanName = document.getElementById('realAkanName');
 
 const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 const femaleNames = ['Akosia', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
@@ -11,7 +11,20 @@ AkanForm.addEventListener('submit', function(event) {
     const birthdate = new Date(document.getElementById('birthdate').value);
     const gender = document.getElementById('gender').value;
 
-    if (!birthdate) || gender === ''){
+    if (!birthdate || gender === '') {
         alert('Please proveide both birthdate and gender');
         return;
     }
+
+    // This is ro claculate the dat of the week
+    const dayOfWeek = birthdate.getDay();
+
+    let realAkanName;
+    if (gender === 'male') {
+        realAkanName = maleNames[dayOfWeek];
+    } else if (gender === 'female') {
+        realAkanName = femaleNames[dayOfWeek];
+    }
+    actualAkanName.textContent = ` ${realAkanName} `;
+
+});
