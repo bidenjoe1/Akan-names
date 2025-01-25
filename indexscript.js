f38797d6712188c0be92e887063a19dfed490fa6
bidenjoe1 <nathan.k.waititu@gmail.com>
2025-01-25 18:45:04 +0300

    Add variables for male and female names

--- indexscript.js
+++ indexscript.js
@@ -1,2 +1,6 @@
 const resultDiv = document.getElementById('result');
 const realAkanName = document.getElementById('realAkanName');
+
+const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+const femaleNames = ['Akosia', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+
