a510cd0a9578e450bda8592010d8ed7bd6247766
bidenjoe1 <nathan.k.waititu@gmail.com>
2025-01-25 19:13:32 +0300

    Add variable to link form, Add event listener and function

--- indexscript.js
+++ indexscript.js
@@ -1,6 +1,10 @@
+const AkanForm = document.getElementById('AkanForm')
 const resultDiv = document.getElementById('result');
 const realAkanName = document.getElementById('realAkanName');
 
 const maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 const femaleNames = ['Akosia', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
+AkanForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+}
